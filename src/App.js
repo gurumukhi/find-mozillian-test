c4c517b28e9064fc1932a33c77a6886dbd5f4c5b
Ram <vaishnav.rd@gmail.com>
2018-04-21 20:06:02 +0530

    test code for find-mozillina

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import fm from 'find-mozillian';
+
+class App extends Component {
+constructor(props){
+    super(props);
+    this.state = {
+        inputQuery: '',
+        result: ''
+    }
+}
+
+onInputChange = (event) => {
+    const input = event.target.value
+    this.setState({
+        inputQuery: input
+    });
+}
+
+clicked = () => {
+    console.log(fm);
+    fm(this.state.inputQuery).then( (val) => {
+        this.setState({
+            result: val
+        });
+    })
+}
+
+
+render() {
+    return (
+    <div style={{ textAlign: 'center', padding: '20px', fontFamily: 'georgia' }}>
+        IsMozillian?
+        <br />
+        <input style={{ margin: '20px' }}
+        type="text" value={this.state.inputQuery} onChange={ this.onInputChange }/>
+        
+        <button onClick = { this.clicked }> Naam </button>
+
+        <div>
+        {this.state.result}
+        </div>
+    </div>
+    );
+}
+}
+
+export default App;
